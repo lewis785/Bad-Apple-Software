3ab92b09b4eff3332a9d6a956f07a1f2ce517d45
Lewis <lewisfmcn@yahoo.co.uk>
2016-01-28 23:33:49 +0000

    qualifications page added

--- Project/HTML/qualifications.php
+++ Project/HTML/qualifications.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-
-<?php 
-include "../PHP/verify.php";
-?>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -11,7 +6,7 @@ include "../PHP/verify.php";
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title> Profile </title>
+    <title> Display Qualifications </title>
     
     <!-- core CSS -->
     <link href="../css/bootstrap.min.css" rel="stylesheet">
@@ -24,6 +19,7 @@ include "../PHP/verify.php";
     <link href="../css/boostrap-theme.css" rel="stylesheet">
     <link href="../css/boostrap-theme.min.css" rel="stylesheet">
     <link href="../css/home.css" rel="stylesheet">
+    <link href="../css/footer.css" rel="styleshet">
     
     <!-- core JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
@@ -37,14 +33,8 @@ include "../PHP/verify.php";
     <script src="../js/Bootstrap/wow.min.js"></script>
     
     <!-- Code for loading user information -->
-    <script src="../js/loadUser.js"></script>
-    <script src="../js/popup.js"></script>
-
 
 </head><!--/head-->
-
-<body onload="loadInfo()">
-
     <div id="navbar">
         <nav id="main-nav" class="navbar navbar-default">
 
@@ -64,21 +54,17 @@ include "../PHP/verify.php";
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
             <ul class="nav navbar-nav navbar-right">
-            <li><a href="addgrades.php"> Add Grades </a></li> 
+                <li><a href="profile.html"> Home </a></li>  
+                
                 <li class="dropdown">
-                  <a href="profile.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profile <span class="caret"></span></a>
+                  <a href="profile.html" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profile <span class="caret"></span></a>
                   <ul class="dropdown-menu">
                     <li><a href="profiledetail.php"> Edit Info </a></li>
                     <li><a href="qualifications.php"> Qualifications </a></li> <!-- still to be made -->
                     <li><a href="#"> Starred Paths </a></li> <!-- still to be made -->
                     <li role="separator" class="divider"></li>
-                    <li><a href="../php/signout.php"> Log out </a></li> <!-- still to be made -->
-
-
                 </ul>
-            </li> 
-            <li><a href="profile.php"> Home </a></li>  
-
+            </li>  
         </ul>
     </li>
 </ul>
@@ -89,56 +75,54 @@ include "../PHP/verify.php";
 
 
 <section id="page-center" class=" col-md-6 col-md-offset-3">
- <div id="center" class="main-section container-fluid">
-     <h2> Your Career Pathways Profile </h2>    
+   <div id="center" class="main-section container-fluid">
+       <h2> Current Qualifications</h2>    
 
-     <div class="row contact-wrap"> 
+       <div class="row contact-wrap"> 
         <div class="col-md-8 col-md-offset-2">
-
-            <ul class="list-group">
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Joined</strong></span> <div id="joined">01/01/01</div> </li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Name</strong></span> <div id="name"> John Smith</div></li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Date of Birth</strong></span> <div id="dob">01/01/01</div> </li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Email</strong></span> <div id="email">example@examp.com</div> </li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong>Address</strong></span> <div id="address">123 Fake Street</div> </li>
-            </ul> 
-
-            <br>    
-            <a id="popin" class="btn btn-primary"> Delete my Account </a>
-            <p> <!-- still to be made -->
-
+        <div class="panel panel-default">
+        <table id="currentQualifications"class="table table-hover">
+            <thead>
+            <tr>
+                <th>Subject</th>
+                <th>Qualification</th>
+                <th>Grade</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Mathematics</td>
+                    <td>Advanced Higher</td>
+                    <td>B</td>
+                </tr>
+                <tr>
+                    <td>English</td>
+                    <td>Higher</td>
+                    <td>A</td>
+                </tr>
+                <tr>
+                    <td>Computing</td>
+                    <td>Higher</td>
+                    <td>A</td>
+                </tr>
+            </tbody>
+            </table>
             </div>
-        </div>
-
-        <div id="popup"> 
-            <a href="../php/delete.php" id="popin" class="btn btn-primary"> Confirm Delete</a>
-            <img id="close" src="../images/close-button.png" class="close-btn"/>
-        </div>
-
-
+            <br>
+            <a id="editQ" class="btn btn-primary"> Edit Qualifications </a>   
+            <a id="back" class="btn btn-primary"> Back </a>
+            <p> <!-- still to be made -->
+            </div>
+            </div>
     </div><!--/.container-->
 </section><!--/#contact-page-->
 
 
 
 <!--/#bottom-->
-<footer id="footer" class="footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6">
+<div id="site-footer" class="site-footer">
                 &copy; 2016 <a target="_blank" href="#" title="badapplesoftware"> Bad Apple Software</a>. All Rights Reserved.
             </div>
-            <div class="col-sm-6">
-                <ul class="pull-right"> Technologies used :
-                    <li>HTML5</li>
-                    <li>CSS3</li>
-                    <li>PHP6</li>
-                    <li>GIMP</li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</footer><!--/#footer-->
 
 </body>
 </html>
