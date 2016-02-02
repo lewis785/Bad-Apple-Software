dd097d6f34a53faa6460067f933130328babc363
Donald Redpath <dkr30@hw.ac.uk>
2016-02-02 14:17:42 +0000

    SQL Query Attempt

--- Project/JS/flatDataConverter.js
+++ Project/JS/flatDataConverter.js
@@ -1,5 +1,5 @@
 
-function getAndConvert(){
+function getAndConvert() {
 
 
     $.ajax({
--- Project/PHP/PathMaker/getUserHistory.php
+++ Project/PHP/PathMaker/getUserHistory.php
@@ -7,21 +7,31 @@ if(!$verified){
 	exit();
 }
 
-$getUser = mysqli_stmt_init($link);
 
-mysqli_stmt_prepare($getUser, "SELECT * FROM userlogin INNER JOIN userdetails ON userlogin.ID = userdetails.UserID
-	INNER JOIN useraddress ON userdetails.Address = useraddress.AddressID
-	WHERE userlogin.UserName = ? and userlogin.Password = ?");
-mysqli_stmt_bind_param($getUser, 'ss', $temp['user'], $temp['pass']);   
-mysqli_stmt_execute($getUser); 
 
-$result = mysqli_stmt_get_result($getUser);
+$getHistory = mysqli_stmt_init($link);
+
+mysqli_stmt_prepare($getHistory, 
+                    
+"SELECT courses.Course AS 'Qualification.Course',
+       levels.Level AS 'Qualification.Level',
+       grades.Grade AS 'Qualification.Grade'
+       
+FROM userqualifications 
+INNER JOIN userlogin ON userqualifications.User = userlogin.UserID
+		INNER JOIN courses ON userqualifications.Course = courses.CourseID
+		INNER JOIN levels ON userqualifications.Level = levels.LevelID
+		INNER JOIN grades ON userqualifications.Grade = grades.GradeID
+		where userlogin.UserName= ? and userlogin.Password = ?
+FOR JSON PATH, ROOT('userqualifications')");  
+mysqli_stmt_execute($getHistory); 
+
+
+$result = mysqli_stmt_get_result($getHistory);
 
 while($row = mysqli_fetch_assoc($result)){
-	echo json_encode(array("user"=>$row['UserName'],"joined"=>$row['DateJoined'],"firstname"=>$row['Firstname'],
-		"surname"=>$row['Surname'],"dob"=>$row['DateOfBirth'],"email"=>$row['EmailAddress'],
-		"number"=>$row['HouseNumberName'], "street"=>$row['StreetName'], "postcode"=>$row['PostCode'],
-		"city"=>$row['City']));
+	echo json_encode($result);
+
 }
 
 ?>
