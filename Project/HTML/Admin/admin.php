be16932060a205c8ec6bbd1eaadba5950869525a
lewis785 <lewisfmcn@yahoo.co.uk>
2016-03-30 12:01:09 +0100

    Admin CSS Change

--- Project/HTML/Admin/admin.php
+++ Project/HTML/Admin/admin.php
@@ -1,38 +1,31 @@
-<!DOCTYPE html>
-
-<?php 
+<?php
 include "../../PHP/Admin/validAdmin.php";
 ?>
-
-<html lang="en"><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-<meta name="description" content="">
-<meta name="author" content="">
-<title>Admin Page</title>
-
-<!-- Bootstrap core CSS -->
-<link href="../../admincss/bootstrap.css" rel="stylesheet">
-
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<link href="../../admincss/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-<link href="../../css/admin.css" rel="stylesheet">
+<html lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Admin Page</title>
+  <!-- Bootstrap core CSS -->
+  <link href="../../admincss/bootstrap.css" rel="stylesheet">
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <link href="../../admincss/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <link href="../../css/admin.css" rel="stylesheet">
 
 
-<!-- Custom styles for this template -->
-<link href="../../admincss/dashboard.css" rel="stylesheet">
-<script src="admincss/ie-emulation-modes-warning.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="../../js/admins.js"></script>
-
+  <!-- Custom styles for this template -->
+  <link href="../../admincss/dashboard.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="../../js/admins.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
-
 <body onload="checkKey()">
-
   <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
       <div id="navbar" class="navbar-collapse collapse">
@@ -45,42 +38,46 @@ include "../../PHP/Admin/validAdmin.php";
       </div>
     </div>
   </nav>
-
   <div class="container-fluid">
     <div class="row">
-      <div class="col-sm-3 col-md-2 sidebar">
-        <ul class="nav nav-sidebar">
-          <li class="active"><a href="admin.php">Overview <span class="sr-only">(current)</span></a></li>
-          <li><a href="manageusers.php">Manage Users</a></li>
-          <li><a href="managewebpage.php">Manage Webpages</a></li>
-          <li><a href="settings.php">Manage Admin Settings</a></li>
-        </ul>
-      </div>
+
+      <nav class="navbar navbar-default sidebar" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          </div>
+          <div class="col-sm-3 col-md-2 sidebar" id="bs-sidebar-navbar-collapse-1">
+            <ul class="nav nav-sidebar">
+              <li class="active"><a href="admin.php">Overview<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+              <li ><a href="manageusers.php">Manage Users<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+              <li ><a href="managewebpage.php">Manage Webpages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-globe"></span></a></li>
+              <li ><a href="settings.php">Manage Admin Settings<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-wrench"></span></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
       <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-        <h1 class="page-header">Administrator Dashboard</h1>
-        
+        <h1 class="page-header">Webpage Dashboard</h1>
+
         <div class="col-lg-12">
           <?php include"../../php/admin/adminstats.php"; ?>
 
+
         </div>
-
-
       </div>
+
     </div>
+
+
   </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="Dashboard%20Template%20for%20Bootstrap_files/jquery.js"></script>
-
-    <script src="Dashboard%20Template%20for%20Bootstrap_files/bootstrap.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="Dashboard%20Template%20for%20Bootstrap_files/holder.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="Dashboard%20Template%20for%20Bootstrap_files/ie10-viewport-bug-workaround.js"></script>
 
 
   </body>
   </html>
+
